becf19ec6fe78e8c81cf6aaa82373fd7cceaadb7
Francisco Mackinlay <franmackinlay@gmail.com>
2021-02-09 11:12:48 -0300

    IMPROVE, login controller

--- app/scripts/controllers/login.controller.js
+++ app/scripts/controllers/login.controller.js
@@ -10,7 +10,7 @@
 
 
 angular.module('bdeoApp')
-  .controller('LoginCtrl', ['$scope', '$rootScope', 'UsersSrv', function (s, r, UsersSrv) {
+  .controller('LoginCtrl', ['$scope', '$rootScope', 'UsersSrv', '$location', function (s, r, UsersSrv, $location) {
     const vm = this;
 
     vm.$onInit = () => {
@@ -20,13 +20,17 @@ angular.module('bdeoApp')
     s.onSubmit = async e => {
       e?.preventDefault();
 
-      if (vm.userEmail) {
-        console.log('USERSRV', UsersSrv);
+      if (vm.userTmp.email && vm.userTmp.password) {
+        const { data } = await UsersSrv.login({ email: vm.userTmp.email, password: vm.userTmp.password });
 
-        const res = await UsersSrv.login({user: vm.userEmail});
-        console.log('RES', res);
+        if (data.token && data.userid) {
+          localStorage.setItem('userInfo', data);
+          $location.path("/sessions");
+        } else {
+          alert('Something went wrong, please try again later! 🧖🏽s')
+        }
       } else {
-        alert('Please enter valid email');
+        alert('Please enter valid email and password');
       }
     }
 
