74dc0ea5ed2c72962daa1646e45068166330afbe
Francisco Mackinlay <franmackinlay@gmail.com>
2021-02-09 11:12:36 -0300

    FIX, users service

--- app/scripts/services/users.service.js
+++ app/scripts/services/users.service.js
@@ -3,7 +3,7 @@ angular
   .factory('UsersSrv', ['$http', function ($http) {
     return {
       login(params) {
-        $http.post(`http://localhost:3000/api/v1/user/login`, params);
+        return $http.post(`http://localhost:3000/api/v1/user/login`, params);
       },
       getUsers(params) {
         return $http.get(`http://localhost:3000/api/v1/user`, { params });
